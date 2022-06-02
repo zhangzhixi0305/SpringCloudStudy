06caf59e82a32be7351c2327f5ff0afa62706df9
zhang-zhixi <1820712542@qq.com>
2022-06-02 12:15:36 +0800

    config-repo

---
+++ config-server/src/main/java/com/zhixi/ConfigApplication.java
@@ -0,0 +1,9 @@
+package com.zhixi;/**
+ * @ClassName ConfigApplication
+ * @Author zhangzhixi
+ * @Description 
+ * @Date 2022-6-2 11:33
+ * @Version 1.0
+ */
+public class ConfigApplication {
+}
---
+++ gateway-server/src/main/java/com/zhixi/GatewayApplication.java
@@ -0,0 +1,9 @@
+package com.zhixi;/**
+ * @ClassName GatewayApplication
+ * @Author zhangzhixi
+ * @Description 
+ * @Date 2022-6-1 18:26
+ * @Version 1.0
+ */
+public class GatewayApplication {
+}
